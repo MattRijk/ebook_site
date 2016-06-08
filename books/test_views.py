0eaeeab5ba00b23c535041c4b00c0ccb6d28abc1
Matt Rijk <matthewrijk@gmail.com>
2016-06-08 12:14:50 -0400

    added test for multiple items in list view

--- books/test_views.py
+++ books/test_views.py
@@ -18,10 +18,39 @@ class ProjectTests(TestCase):
         self.assertContains(response, 'Corporate Finance')
         self.assertContains(response, '2003')
         
-#     def test_two_books(self):
-#         Book.objects.create(title='1-title', body='1-body', author=)
-#         Book.objects.create(title='2-title', body='2-body', author=)
-#         response = self.client.get('/')
-#         self.assertContains(response, '1-title')
-#         self.assertContains(response, '1-body')
-#         self.assertContains(response, '2-title')
+    def test_two_books_with_authors(self):
+        
+        a1 = Author.objects.create(name = 'Scott Meyer')
+        a2 = Author.objects.create(name = 'Sarah Green')
+        a3 = Author.objects.create(name = 'Jennifer Rose')
+        a4 = Author.objects.create(name = 'Jose Rogers')
+        
+        image1 = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
+        pdf1 = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
+        b1 = Book.objects.create(title='Corporate Finance', pages='272', image=image1, pdf=pdf1, year='2003', filesize='1.62 MB', file_format='PDF')
+        image2 = File(open(r'C:\Users\matt\Desktop\temp\applied_quantitive_finance.jpg', mode="rb"))
+        pdf2 = File(open(r'C:\Users\matt\Desktop\temp\applied_quantitive_finance.pdf', mode="rb"))
+        b2 = Book.objects.create(title='Applied Quantitative Finance', pages='411', image=image2, pdf=pdf2, year='2009', filesize='2.25 MB', file_format='PDF')
+        
+        scott = BookHasAuthor.objects.create(book=b1, author=a1)
+        sarah = BookHasAuthor.objects.create(book=b1, author=a2)
+        
+        jennifer = BookHasAuthor.objects.create(book=b2, author=a3)
+        jose = BookHasAuthor.objects.create(book=b2, author=a4)
+        scott = BookHasAuthor.objects.create(book=b2, author=a1)
+        
+        response = self.client.get('/books/')
+
+        self.assertContains(response, 'Corporate Finance')
+        self.assertContains(response, '2003')
+        self.assertContains(response, 'Scott Meyer')
+        self.assertContains(response, 'Sarah Green')
+        
+        self.assertContains(response, 'Applied Quantitative Finance')
+        self.assertContains(response, '2009')
+        self.assertContains(response, 'Jennifer Rose')
+        self.assertContains(response, 'Jose Rogers')
+        self.assertContains(response, 'Scott Meyer')
+   
+        
+ 
