61074350cb74dd51598b0494775d2c098f39e26f
Matt Rijk <matthewrijk@gmail.com>
2016-06-10 14:59:04 -0400

    added view tests for categoryview

--- books/test_views.py
+++ books/test_views.py
@@ -2,7 +2,7 @@ from django.test import TestCase, testcases
 from django.core.files import File
 from django.template import Template, Context
 from django.core.urlresolvers import reverse, resolve
-from books.models import Book, Author, BookHasAuthor, Category
+from books.models import Book, Author, BookHasAuthor, Category, BookHasCategory
 from books.templatetags import navtags
 
 
@@ -23,7 +23,7 @@ class BookListViewTests(TestCase):
         self.assertContains(response, 'Corporate Finance')
         self.assertContains(response, '2003')
         
-    def test_two_books_with_authors(self):
+    def test_two_books_with_two_authors(self):
         
         a1 = Author.objects.create(name = 'Scott Meyer')
         a2 = Author.objects.create(name = 'Sarah Green')
@@ -64,31 +64,32 @@ class BookListViewTests(TestCase):
         response = self.client.get(book.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         
-    def test_author_get__url(self):
+    def test_author_get_url(self):
         author = Author.objects.create(name = 'Scott Meyer')
         image = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
         pdf = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
         book =  Book.objects.create(title='Corporate Finance', pages='272', image=image, pdf=pdf, year='2003', filesize='1.62 MB', file_format='PDF')
          
         BookHasAuthor.objects.create(book=book, author=author)
-#          response = self.client.get('author/%s/' % author.slug)
         author= resolve('/author/scott-meyer/')
         self.assertEqual(author.kwargs['slug'], 'scott-meyer')
         
-    def test_author_url(self):
+    def test_author_url_success(self):
         author = Author.objects.create(name = 'Scott Meyer')
         image = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
         pdf = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
         book =  Book.objects.create(title='Corporate Finance', pages='272', image=image, pdf=pdf, year='2003', filesize='1.62 MB', file_format='PDF')
-         
+        
         BookHasAuthor.objects.create(book=book, author=author)
+        
         response = self.client.get('/author/%s/' % (author.slug,))
+        
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'books/author_books.html')
         
         
 class BookDetailViewTest(TestCase):
-    
+    # test book get_absolute_url
     def book_detail_page(self):
         image = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
         pdf = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
@@ -107,9 +108,86 @@ class CategoryNavTagTest(TestCase):
         self.assertIn(category.title, rendered)
         
 class CategoryDetailViewTest(TestCase):
-    pass
-
-         
+    
+    
+    # returns category slug kwargs 
+    def test_category_url_with_slug(self):
+ 
+              
+        category = Category.objects.create(title='Stock Investing')
+        image = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
+        pdf = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
+        book =  Book.objects.create(title='Corporate Finance', pages='272', image=image, pdf=pdf, year='2003', filesize='1.62 MB', file_format='PDF')
+        
+        BookHasCategory.objects.create(book=book, category=category)
+        category= resolve('/category/stock-investing/')
+        self.assertEqual(category.kwargs['slug'], 'stock-investing')
+        
+    
+    # returns books/book_category.html with '/category/%s/' % (category.slug,)
+    def test_author_url_success(self):
+        category = Category.objects.create(title = 'Stock Investing')
+        image = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
+        pdf = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
+        book =  Book.objects.create(title='Corporate Finance', pages='272', image=image, pdf=pdf, year='2003', filesize='1.62 MB', file_format='PDF')
+        
+        BookHasCategory.objects.create(book=book, category=category)
+        
+        response = self.client.get('/category/%s/' % (category.slug,))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'books/book_category.html')
+        
+    
+        def test_two_books_with_two_authors_each_on_category_page(self):
+            # Book 1
+            #2 authors
+            a1 = Author.objects.create(name = 'Scott Meyer')
+            a2 = Author.objects.create(name = 'Sarah Green')
+            # 1 category
+            c1 = Category.objects.create(name='Quant Investing')
+            ##Book
+            image1 = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.jpg', mode="rb"))
+            pdf1 = File(open(r'C:\Users\matt\Desktop\temp\corporate_finance_theory_and_practice.pdf', mode="rb"))
+            b1 = Book.objects.create(title='Corporate Finance', pages='272', image=image1, pdf=pdf1, year='2003', filesize='1.62 MB', file_format='PDF')
+            
+            BookHasAuthor.objects.create(book=b1, author=a1)
+            BookHasAuthor.objects.create(book=b1, author=a2)
+            
+            BookHasCategory.objects.create(book=b1, category=c1)
+            
+            #Book 2
+            #2 authors
+            a3 = Author.objects.create(name = 'Jennifer Rose')
+            a4 = Author.objects.create(name = 'Jose Rogers')
+            ## Book
+            image2 = File(open(r'C:\Users\matt\Desktop\temp\applied_quantitive_finance.jpg', mode="rb"))
+            pdf2 = File(open(r'C:\Users\matt\Desktop\temp\applied_quantitive_finance.pdf', mode="rb"))
+            b2 = Book.objects.create(title='Applied Quantitative Finance', pages='411', image=image2, pdf=pdf2, year='2009', filesize='2.25 MB', file_format='PDF')
+        
+            BookHasAuthor.objects.create(book=b2, author=a3)
+            BookHasAuthor.objects.create(book=b2, author=a4)
+            # One Book: Two authors, one category
+            BookHasCategory.objects.create(book=b2, category=c1)
+           
+            
+            response = self.client.get('/category/quant-investing/')
+    
+            self.assertContains(response, 'Quant Investing')
+            
+            self.assertContains(response, 'Corporate Finance')
+            self.assertContains(response, '2003')
+            
+            self.assertContains(response, 'Scott Meyer')
+            self.assertContains(response, 'Sarah Green')
+            
+            self.assertContains(response, 'Applied Quantitative Finance')
+            self.assertContains(response, '2009')
+            
+            self.assertContains(response, 'Jennifer Rose')
+            self.assertContains(response, 'Jose Rogers')
+        
+             
        
    
         
